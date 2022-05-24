0fef1f5157536d5b47ced0e03519bff744d16a6c
Samuel Hinga <samuelhinga200@gmail.com>
2022-05-25 00:45:47 +0300

    Home components structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>ZURU</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="zuru BnB" />
+      </Helmet>
+      <h1 className="text-3xl font-bold">Hello Badeh!</h1>
     </div>
   );
 }
